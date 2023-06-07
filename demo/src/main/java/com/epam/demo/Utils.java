f5eb73af00a022678b72b6ccc3df3af846060053
Mustafoyev <mmustafoyev9@gmail.com>
2023-06-07 09:01:24 +0500

    commit message

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,24 @@
 package com.epam.demo;
 
+
+
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        //magic happens here
+        StringUtils obj = new StringUtils();
+        for (String str : args) {
+            if(!obj.isPositiveNumber(str))
+                return false;
+        }
+        return false;
     }
+
+//    public static void main(String[] args) {
+//        List<String> list =  List.of("-11.22", "0", "33", "87");
+//        System.out.println(isAllPositiveNumbers(list));
+//    }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,20 @@
 package com.epam.utils;
 
+
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        try{
+            return (NumberUtils.toDouble(str) > 0.0 && str.charAt(0) != '0');        }
+        catch (Exception e){
+            return false;
+        }
+
         //code!
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber("34435"));
     }
 }
